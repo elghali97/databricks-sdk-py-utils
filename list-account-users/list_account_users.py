fc90c9e9f98c3c276a588c0c9e8ddb5b351a9ee2
Ghali Elalaoui Elabdellaoui <56326095+ghaliela@users.noreply.github.com>
2024-08-28 11:37:48 +0200

    improve El Ghali's code

--- list-account-users/list_account_users.py
+++ list-account-users/list_account_users.py
@@ -4,19 +4,27 @@ from databricks.sdk.oauth import SessionCredentials
 
 import pandas as pd
 
-if __name__ == "__main__":
-    # For account-level operations, you should first use the Databricks CLI to run the following command, before you run your Python code. 
-    # This command instructs the Databricks CLI to generate and cache the necessary OAuth token in the path .databricks/token-cache.json 
-    # within your user’s home folder on your machine:
+# Optional: create an enum for possible attributes
+ATTRIBUTES = ['userName', 'display_name', 'roles', 'groups'] # To refactor databricks sdk, userName in camel case but display_name in snake case
 
-    # databricks auth login --host https://accounts.azuredatabricks.net/ --account-id <account-id>
+if __name__ == "__main__":
+    # Use docstring instead of comment
+    """For account-level operations, you should first use the Databricks CLI to run the following command, before you run your Python code. 
+    This command instructs the Databricks CLI to generate and cache the necessary OAuth token in the path .databricks/token-cache.json 
+    within your user’s home folder on your machine:
+
+    databricks auth login --host https://accounts.azuredatabricks.net/ --account-id <account-id>
+    """
 
     profile = input("Enter Databricks host profile: ")
-    a = AccountClient(profile=profile)
-    all_users = a.users.list(attributes="userName,display_name,roles,groups",
+    account = AccountClient(profile=profile)
+    all_users = account.users.list(attributes=','.join(ATTRIBUTES),
                             sort_by="userName",
                             sort_order=iam.ListSortOrder.DESCENDING)
 
     users = [user.as_dict() for user in all_users]
     df = pd.DataFrame(users)
-    print(df)
+
+    # Avoid using print
+    df.describe()
+    df.head()
